f30aa2a2fffdbe941604ebb57da435ef691674ed
Mitchell Gardepe <mitchellgardepe@Mitchells-MacBook-Pro.local>
2019-06-14 10:32:37 -0400

    first commit. Made a rough login page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { MatButtonModule, MatInputModule } from "@angular/material";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { Router } from "@angular/router";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styles: []
+})
+export class LoginComponent implements OnInit {
+  username = new FormControl("");
+  password = new FormControl("");
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    console.log(this.username);
+    console.log(this.password);
+  }
+
+  authenticate() {
+    if (this.username.value != "" && this.password.value != "") {
+      this.router.navigateByUrl("home");
+    }
+  }
+}
